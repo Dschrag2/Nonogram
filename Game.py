0d4c3c93e0deee3d2d2887616517064ed861759a
dschrag2 <dschrag2@nd.edu>
2023-12-28 21:52:36 -0500

    Finished Game.py

--- Game.py
+++ Game.py
@@ -6,10 +6,11 @@ import random
 # Defining Globals
 BOARD_SIZE = 10
 SQUARE_SIZE = 40
-FILLER = 80
+FILLER = 100
 BLACK = (0,0,0)
 GRAY = (130,130,130)
 WHITE = (255,255,255)
+FONT_SIZE = 30
 
 # Square class
 class Square:
@@ -28,14 +29,71 @@ class Square:
             pygame.draw.line(screen, GRAY, (self.x+1, self.y+1), (self.x+SQUARE_SIZE-2, self.y+SQUARE_SIZE-2))
             pygame.draw.line(screen, GRAY, (self.x+SQUARE_SIZE-2, self.y+1), (self.x+1, self.y+SQUARE_SIZE-2))
 
-def update_display(screen,squares):
+def get_labels(squares,font):
+    rows, cols = [],[]
+    # Getting Column Labels
+    for col in squares:
+        temp,count = [],0
+        for i,square in enumerate(col):
+            if square.value == 1:
+                count += 1
+                if i == BOARD_SIZE-1:
+                    temp.append(str(count))
+            else:
+                if count != 0:
+                    temp.append(str(count))
+                    count = 0
+        cols.append(temp)
+    # Getting Row Labels
+    for j in range(BOARD_SIZE):
+        temp,count = [],0
+        for i in range(BOARD_SIZE):
+            if squares[i][j].value == 1:
+                count += 1
+                if i == BOARD_SIZE-1:
+                    temp.append(str(count))
+            else:
+                if count != 0:
+                    temp.append(str(count))
+                    count = 0
+        rows.append(temp)
+    # Getting Row Label Surfaces
+    rows = [" ".join(row) for row in rows]
+    row_surfaces = [font.render(row, True, BLACK) for row in rows]
+    # Getting Column Label Surfaces
+    cols = ["".join(col) for col in cols]
+    col_surfaces = [[font.render(char, True, BLACK) for char in col] for col in cols]
+    return row_surfaces,col_surfaces
+
+def update_display(screen,squares,row_surf,col_surf):
     # Filling background
     screen.fill(WHITE)
+
+    # Drawing Extra Lines
+    for i in range(BOARD_SIZE):
+        pygame.draw.line(screen, BLACK, (0,FILLER+SQUARE_SIZE*i),(FILLER,FILLER+SQUARE_SIZE*i))
+        pygame.draw.line(screen, BLACK, (FILLER+SQUARE_SIZE*i,0),(FILLER+SQUARE_SIZE*i,FILLER))
 
     # Displaying each square
     for row in squares:
         for square in row:
             square.display(screen)
+
+    # Displaying each row label
+    x = 0
+    y = FILLER + SQUARE_SIZE/4
+    for row in row_surf:
+        screen.blit(row, (x,y))
+        y += SQUARE_SIZE
+
+    x = FILLER - 3*SQUARE_SIZE/4
+    for i in range(len(col_surf)):
+        x += SQUARE_SIZE
+        y = 0
+        for char in col_surf[i]:
+            screen.blit(char, (x,y))
+            y += char.get_height()
+
     pygame.display.flip()
 
 def handle_events(squares):
@@ -63,16 +121,21 @@ def handle_events(squares):
 def main():
     # Initializing Pygame
     pygame.init()
+    pygame.font.init()
     screen = pygame.display.set_mode((SQUARE_SIZE*BOARD_SIZE+FILLER,SQUARE_SIZE*BOARD_SIZE+FILLER))
+    font = pygame.font.Font(None, FONT_SIZE)
 
     # Creating square array
     squares = [[Square(FILLER + i * SQUARE_SIZE, FILLER + j * SQUARE_SIZE) for j in range(BOARD_SIZE)] for i in range(BOARD_SIZE)]
     
+    # Getting labels
+    rowLabel,colLabel = get_labels(squares,font)
+
     # Creating game loop
     run = True
     while run:
         handle_events(squares)
-        update_display(screen,squares)
+        update_display(screen,squares,rowLabel,colLabel)
 
 if __name__ == "__main__":
     main()
